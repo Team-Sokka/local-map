2f6a01951dfd4013f0131da56c0bd80a0eae4356
Quinn Royston <quinn.royston@gmail.com>
2020-06-17 16:02:04 -0700

    Installed dotenv as a dependency to create the port as a process.env variable, and store other configuration variables

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const app = express()
-const port = 8000;
+const port = process.env.PORT;
 
 app.use(morgan('tiny'));
 app.set('query parser', 'simple');
@@ -11,6 +12,7 @@ app.get('/', (req, res) => {
   console.log('query params', req.query)
   res.send('Hello World');
 })
+
 
 app.listen(port, ()=>{
   console.log(`Listening at http://127.0.0.1:${port}`)
