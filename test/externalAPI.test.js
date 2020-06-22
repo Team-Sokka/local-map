775e3325fdfb32f64c90d07c437a9419edc85a38
Quinn Royston <quinn.royston@gmail.com>
2020-06-22 15:14:38 -0700

    Smoke test added

--- test/externalAPI.test.js
+++ test/externalAPI.test.js
@@ -1,6 +1,9 @@
 const externalAPI = require('../server/externalAPI.js')
 require('dotenv').config();
 
+test('Yelp Request function exists', () => {
+  expect(typeof externalAPI.yelp === 'function').toBe(true);
+})
 // test('Yelp Request Returns Data', () => {
 //   var lat = "21.260088";
 //   var lng = "-157.706806";
