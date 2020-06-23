0c4be2de3b4e0fc5b49ec7ec3a93be1f3708cda8
Quinn Royston <quinn.royston@gmail.com>
2020-06-23 11:07:21 -0700

    Updated Package-json, and added back the test for the Yelp API

--- test/externalAPI.test.js
+++ test/externalAPI.test.js
@@ -1,13 +1,13 @@
 const externalAPI = require('../server/externalAPI.js')
 require('dotenv').config();
 
-test('Yelp Request function exists', () => {
-  expect(typeof externalAPI.yelp === 'function').toBe(true);
-})
-// test('Yelp Request Returns Data', () => {
-//   var lat = "21.260088";
-//   var lng = "-157.706806";
-//   return externalAPI.yelp(lat, lng).then(data => {
-//     expect(Array.isArray(data.data.businesses)).toBe(true);
-//     })
+// test('Yelp Request function exists', () => {
+//   expect(typeof externalAPI.yelp === 'function').toBe(true);
 // })
+test('Yelp Request Returns Data', () => {
+  var lat = "21.260088";
+  var lng = "-157.706806";
+  return externalAPI.yelp(lat, lng).then(data => {
+    expect(Array.isArray(data.data.businesses)).toBe(true);
+    })
+})
