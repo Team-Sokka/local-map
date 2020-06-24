dabd5e846be48665bd1a5b9a5fce792b0c2c50c0
Quinn Royston <quinn.royston@gmail.com>
2020-06-24 09:27:33 -0700

    update yelp function to perform a search for each category

--- server/externalAPI.js
+++ server/externalAPI.js
@@ -1,21 +1,46 @@
 const axios = require('axios').default;
-var yelp = (lat, lng, categories="Restaurants, Shopping, ArtsAndEntertainment, Fitness") => {
-  //ForDebugging purposes
-  // console.log('Yelping!')
-  // console.log('lat - ', lat)
-  // console.log('lng - ', lng)
-  // console.log('categories', categories)
-  // console.log(process.env.YELP_API_KEY)
-  return axios.get('https://api.yelp.com/v3/businesses/search', {
-    params: {
-      latitude: lat,
-      longitude: lng,
-      categories:categories
-    },
-    headers: {
-      "Authorization": `Bearer ${process.env.YELP_API_KEY}`
-    }
+// var yelp = (lat, lng, categories="restaurants, shopping, arts, fitness") => {
+//   //ForDebugging purposes
+//   // console.log('Yelping!')
+//   // console.log('lat - ', lat)
+//   // console.log('lng - ', lng)
+//   // console.log('categories', categories)
+//   // console.log(process.env.YELP_API_KEY)
+//   console.log('Categories - ', categories)
+//   return axios.get('https://api.yelp.com/v3/businesses/search', {
+//     params: {
+//       latitude: lat,
+//       longitude: lng,
+//       categories:categories,
+//       limit: 50
+//     },
+//     headers: {
+//       "Authorization": `Bearer ${process.env.YELP_API_KEY}`
+//     }
+//   })
+// }
+var yelp = (lat, lng, categories='restaurants, shopping, arts, fitness') => {
+  var categoryArr = categories.split(',');
+  //console.log('Category Array - ', categoryArr)
+  //categoryArr
+  var searches = categoryArr.map((category) => {
+    return axios.get('https://api.yelp.com/v3/businesses/search', {
+      params: {
+        latitude: lat,
+        longitude: lng,
+        categories: category,
+        limit: 50
+      },
+      headers: {
+        "Authorization": `Bearer ${process.env.YELP_API_KEY}`
+      }
+    });
+  });
+  var results = Promise.all(searches).then((data) => {
+    var categorySearches = data.map((search) => {return search.data})
+    return categorySearches;
   })
+  return results;
 }
 
 module.exports.yelp = yelp;
