5ddd895f9fe7d341cc52220706392319f2f13087
Quinn Royston <quinn.royston@gmail.com>
2020-06-24 10:33:36 -0700

    Attempted further configuration, by install jest-enzyme, jest-environment-enzyme, and using enzyme-adapter-react-16

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
+
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -123,7 +124,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  //setupFilesAfterEnv: ["./node_modules/jest-enzyme/lib/index.js"],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
--- test/app.test.js
+++ test/app.test.js
@@ -1,25 +1,27 @@
-const enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16.3');
-import sinon from 'sinon';
+import React from 'react';
+import {configure, shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../client/App.jsx';
+import Modal from '../client/components/Modal.jsx';
+//const Adapter = require('enzyme-adapter-react-16');
 
-enzyme.configure({adapter: new Adapter()});
+configure({adapter: new Adapter()});
 
-const App = require('../client/App.jsx');
-const Modal = require('../client/components/Modal.jsx')
+//const App = require('../client/App.jsx');
+//const Modal = require('../client/components/Modal.jsx')
 
 //console.log('enzyme - ', enzyme)
 //console.log('Adapter - ',Adapter)
 //import Adapter from 'enzyme-adapter-react-16.3';
 
 describe('App Component tests', ()=> {
-
   test('App Component Renders', ()=>{
-    const wrapper = enzyme.shallow(<App/>);
+    const wrapper = shallow(<App />);
     expect(wrapper).toBe(true)
   });
 
   test('Modal Component renders', ()=>{
-    const wrapper = enzyme.shallow(<App/>);
+    const wrapper = shallow(<App/>);
     expect(wrapper.find(Modal)).to.have.lengthOf(1);
   })
 })
