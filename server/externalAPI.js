44395716f78debdcfa60c6160dd3c7e301361333
Quinn Royston <quinn.royston@gmail.com>
2020-06-25 16:27:50 -0700

    Removed unnecessary comments

--- server/externalAPI.js
+++ server/externalAPI.js
@@ -1,28 +1,6 @@
 const axios = require('axios').default;
-// var yelp = (lat, lng, categories="restaurants, shopping, arts, fitness") => {
-//   //ForDebugging purposes
-//   // console.log('Yelping!')
-//   // console.log('lat - ', lat)
-//   // console.log('lng - ', lng)
-//   // console.log('categories', categories)
-//   // console.log(process.env.YELP_API_KEY)
-//   console.log('Categories - ', categories)
-//   return axios.get('https://api.yelp.com/v3/businesses/search', {
-//     params: {
-//       latitude: lat,
-//       longitude: lng,
-//       categories:categories,
-//       limit: 50
-//     },
-//     headers: {
-//       "Authorization": `Bearer ${process.env.YELP_API_KEY}`
-//     }
-//   })
-// }
 var yelp = (lat, lng, categories='restaurants, shopping, arts, fitness') => {
   var categoryArr = categories.split(',');
-  //console.log('Category Array - ', categoryArr)
-  //categoryArr
   var searches = categoryArr.map((category) => {
     return axios.get('https://api.yelp.com/v3/businesses/search', {
       params: {
