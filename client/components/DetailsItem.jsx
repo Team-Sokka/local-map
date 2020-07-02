a80a153fd273f176047c1176ae19b171991639f0
Quinn Royston <quinn.royston@gmail.com>
2020-07-01 17:44:28 -0700

    Replaced assets in DetailsItem with s3 Assets

--- client/components/DetailsItem.jsx
+++ client/components/DetailsItem.jsx
@@ -19,7 +19,7 @@ const DetailsItem = (props) => (
         <img src={ratingImages[props.business.rating]}/> {props.business.review_count} Reviews
       </div>
       <div>
-        <a target="_blank" href={props.business.url}><img src="https://www.trulia.com/images/txl/icons/yelp/yelp_logo_small.png" style={{width: '40px'}}/></a>
+        <a target="_blank" href={props.business.url}><img src="https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/yelp_logo_small.png" style={{width: '40px'}}/></a>
       </div>
       </ReviewsAndRating>
 
@@ -29,16 +29,16 @@ const DetailsItem = (props) => (
 </ItemContainer>
 )
 var ratingImages = {
-  '0': 'https://www.trulia.com/images/txl/icons/yelp/small_0.png',
-  '1': 'https://www.trulia.com/images/txl/icons/yelp/small_1.png',
-  '1.5': 'https://www.trulia.com/images/txl/icons/yelp/small_1_half.png',
-  '2': 'https://www.trulia.com/images/txl/icons/yelp/small_2.png',
-  '2.5': 'https://www.trulia.com/images/txl/icons/yelp/small_2_half.png',
-  '3': 'https://www.trulia.com/images/txl/icons/yelp/small_3.png',
-  '3.5': 'https://www.trulia.com/images/txl/icons/yelp/small_3_half.png',
-  '4': 'https://www.trulia.com/images/txl/icons/yelp/small_4.png',
-  '4.5': 'https://www.trulia.com/images/txl/icons/yelp/small_4_half.png',
-  '5': 'https://www.trulia.com/images/txl/icons/yelp/small_5.png'
+  '0': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_0.png',
+  '1': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_1.png',
+  '1.5': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_1_half.png',
+  '2': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_2.png',
+  '2.5': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_2_half.png',
+  '3': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_3.png',
+  '3.5': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_3_half.png',
+  '4': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_4.png',
+  '4.5': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_4_half.png',
+  '5': 'https://hrr46-fec-localmap-bucket.s3.amazonaws.com/yelp/small_5.png'
 }
 const ItemContainer = styled.div`
 background-color: white;
